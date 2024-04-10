0e7c062332fe811b1d3efe573f3f4bdcb2ca84b6
SpliiT <spliitdev@gmail.com>
2024-04-10 10:04:09 +0200

    added comments

--- js/script.js
+++ js/script.js
@@ -1,30 +1,33 @@
 document.getElementById("themeSwitch").addEventListener("change", function () {
-  img = document.getElementById("gameImage");
-  github = document.getElementById("github");
-  document.body.classList.toggle("dark-theme", this.checked);
+  img = document.getElementById("gameImage"); // Sélection de l'élément image du jeu
+  github = document.getElementById("github"); // Sélection de l'élément github
+  document.body.classList.toggle("dark-theme", this.checked); // Basculement du thème sombre
 
   if (document.body.classList.contains("dark-theme")) {
-    github.src = "/assets/img/github2.png";
-    img.src = "/assets/img/easteregg.png";
-    img.id = "gameImage";
-    img.onclick = function() {
-        choisirOption('jul');
+    // Si le thème sombre est activé
+    github.src = "/assets/img/github2.png"; // Changement de l'image github
+    img.src = "/assets/img/easteregg.png"; // Changement de l'image du jeu
+    img.id = "gameImage"; // Modification de l'ID de l'image
+    img.onclick = function () {
+      choisirOption("jul"); // Appel de la fonction choisirOption avec l'option 'jul'
     };
   } else {
-    img.src = "/assets/img/rock.png";
-    github.src = "/assets/img/github.png";
+    // Si le thème sombre n'est pas activé
+    img.src = "/assets/img/rock.png"; // Changement de l'image du jeu
+    github.src = "/assets/img/github.png"; // Changement de l'image github
 
-    img.onclick = function() {
-      choisirOption('puit');
+    img.onclick = function () {
+      choisirOption("puit"); // Appel de la fonction choisirOption avec l'option 'puit'
     };
   }
 });
 
-let customDivs = document.querySelectorAll(".one", ".logo", ".easteregg");
+let customDivs = document.querySelectorAll(".one", ".logo", ".easteregg"); // Sélection de certains éléments div personnalisés
 customDivs.forEach(function (div) {
   div.addEventListener("click", function () {
     if (document.getElementById("themeSwitch").checked) {
-      div.classList.toggle("dark-theme");
+      // Si le thème sombre est activé
+      div.classList.toggle("dark-theme"); // Basculement du thème sombre pour l'élément div
     }
   });
 });
--- js/shifumi.js
+++ js/shifumi.js
@@ -1,36 +1,42 @@
+// Définition des variables et constantes
 const choix = ["pierre", "papier", "ciseaux", "puit", "jul"];
-let scoreJoueur = 0;
-let scoreOrdinateur = 0;
-let nombreVictoiresNecessaires = 3;
+let scoreJoueur = 0; // Initialisation du score du joueur
+let scoreOrdinateur = 0; // Initialisation du score de l'ordinateur
+let nombreVictoiresNecessaires = 3; // Nombre de victoires nécessaires pour gagner
 
+// Fonction pour changer le mode de jeu
 function changerModeJeu() {
   const selectMode = document.getElementById("mode");
-  nombreVictoiresNecessaires = parseInt(selectMode.value);
-  réinitialiserScores();
+  nombreVictoiresNecessaires = parseInt(selectMode.value); // Mettre à jour le nombre de victoires nécessaires
+  réinitialiserScores(); // Réinitialiser les scores
 }
 
+// Fonction pour réinitialiser les scores
 function réinitialiserScores() {
   scoreJoueur = 0;
   scoreOrdinateur = 0;
-  document.getElementById("score").innerText = `Joueur ${scoreJoueur} - ${scoreOrdinateur} Ordinateur`;
+  document.getElementById(
+    "score"
+  ).innerText = `Joueur ${scoreJoueur} - ${scoreOrdinateur} Ordinateur`;
   document.getElementById("score").style.display = "none"; // Masquer la section du score
 }
 
+// Fonction pour que l'ordinateur fasse un choix aléatoire
 function PrendreChoixOrdinateur(choixjoueur) {
   const randomIndex = Math.floor(Math.random() * 3);
 
-  if (
-    document.body.classList.contains("dark-theme") &&
-    choixjoueur === "jul"
-  ) {
+  if (document.body.classList.contains("dark-theme") && choixjoueur === "jul") {
     return "jul";
   } else {
     return choix[randomIndex];
   }
 }
 
+// Fonction pour comparer les choix du joueur et de l'ordinateur et déterminer le gagnant
 function choisirOption(choixjoueur) {
   const choixordinateur = PrendreChoixOrdinateur(choixjoueur);
+
+  // Affichage des choix du joueur et de l'ordinateur
   document.getElementById(
     "joueur"
   ).innerHTML = `<h3 style="margin-top: 30px">Joueur 👤</h3><img src="assets/img/${choixjoueur}j.png">`;
@@ -38,7 +44,12 @@ function choisirOption(choixjoueur) {
     "ordinateur"
   ).innerHTML = `<h3 style="margin-top: 30px">Ordinateur 🤖</h3><img src="assets/img/${choixordinateur}o.png">`;
 
-  if (choixjoueur === "papier" && choixordinateur === "papier" || choixjoueur === "pierre" && choixordinateur === "pierre" || choixjoueur === "ciseaux" && choixordinateur === "ciseaux") {
+  // Logique pour déterminer le résultat du jeu
+  if (
+    (choixjoueur === "papier" && choixordinateur === "papier") ||
+    (choixjoueur === "pierre" && choixordinateur === "pierre") ||
+    (choixjoueur === "ciseaux" && choixordinateur === "ciseaux")
+  ) {
     document.getElementById("resultat-text").innerText = "Égalité 🤝";
   } else if (
     (choixjoueur === "pierre" && choixordinateur === "ciseaux") ||
@@ -67,38 +78,44 @@ function choisirOption(choixjoueur) {
     scoreOrdinateur++;
   }
 
-  document.getElementById("score").innerHTML = `Joueur ${scoreJoueur} - ${scoreOrdinateur} Ordinateur<br>`;
+  document.getElementById(
+    "score"
+  ).innerHTML = `Joueur ${scoreJoueur} - ${scoreOrdinateur} Ordinateur<br>`;
 
   if (document.getElementById("score").style.display === "none") {
     document.getElementById("score").style.display = "block";
   }
 
+  // Vérifier s'il y a un gagnant et afficher un message approprié
   if (scoreJoueur === nombreVictoiresNecessaires) {
     afficherMessageVictoireJoueur(``);
     réinitialiserScores();
-} else if (scoreOrdinateur === nombreVictoiresNecessaires) {
+  } else if (scoreOrdinateur === nombreVictoiresNecessaires) {
     afficherMessageVictoireOrdi(``);
     réinitialiserScores();
+  }
 }
 
+// Fonction pour afficher un message de victoire pour le joueur
 function afficherMessageVictoireJoueur(message) {
-    const resultatContainer = document.getElementById("resultat-text");
-    resultatContainer.innerText = message;
+  const resultatContainer = document.getElementById("resultat-text");
+  resultatContainer.innerText = message;
 
-    const imageElement = document.createElement("img");
-    imageElement.src = "assets/img/winjoueur.png"; 
-    imageElement.style.width = "300px"; // 
+  const imageElement = document.createElement("img");
+  imageElement.src = "assets/img/winjoueur.png";
+  imageElement.style.width = "300px"; // Ajuster la largeur de l'image
 
-    resultatContainer.appendChild(imageElement);
+  resultatContainer.appendChild(imageElement);
 }
-}
+
+// Fonction pour afficher un message de victoire pour l'ordinateur
 function afficherMessageVictoireOrdi(message) {
   const resultatContainer = document.getElementById("resultat-text");
   resultatContainer.innerText = message;
 
   const imageElement = document.createElement("img");
-  imageElement.src = "assets/img/winordi.png"; 
-  imageElement.style.width = "300px"; // 
+  imageElement.src = "assets/img/winordi.png";
+  imageElement.style.width = "300px"; // Ajuster la largeur de l'image
 
   resultatContainer.appendChild(imageElement);
 }
